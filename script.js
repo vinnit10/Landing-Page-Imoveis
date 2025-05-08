48e68e8efe262c78d902dddea58bbf0ef8793b18
Vinicius Teixeira <137602744+vinnit10@users.noreply.github.com>
2025-05-08 15:10:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Scroll suave (caso use âncoras)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
